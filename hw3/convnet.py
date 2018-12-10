5f565a2bb2cc497dd84e221a91ee2f9ac2088bb5
slawiko <shchaurouski.slava@gmail.com>
2018-12-10 19:42:26 +0300

    NesterovMomentum -> Adam = 5 percents boost

--- hw3/convnet.py
+++ hw3/convnet.py
@@ -101,7 +101,7 @@ def cnn_model_fn(features, labels, mode):
 
     # Configure the Training Op (for TRAIN mode)
     if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.MomentumOptimizer(learning_rate=0.001, momentum=0.9, use_nesterov=True)
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
         train_op = optimizer.minimize(
             loss=loss,
             global_step=tf.train.get_global_step())
